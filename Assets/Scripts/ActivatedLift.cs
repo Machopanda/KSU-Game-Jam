eab401360ea5bd8a80e7f3fcea41bc6a8d7f76ea
Machopanda <skoochypumpum@gmail.com>
2025-09-27 14:46:09 -0400

    Button Lift
    Button Lift

--- Assets/Scripts/ActivatedLift.cs
+++ Assets/Scripts/ActivatedLift.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class ActivatedLift : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float liftDistance = 5f; // maximum distance to move up
+    public float speed = 2f; // movement speed
+
+    private float initialY;   // starting Y position
+    private float targetY;    // current target position
+    private bool movingUp = false;
+    private bool movingDown = false;
+
     void Start()
     {
-        
+        initialY = transform.position.y;
+        targetY = initialY;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (movingUp)
+        {
+            targetY = Mathf.Min(transform.position.y + speed * Time.deltaTime, initialY + liftDistance);
+            transform.position = new Vector3(transform.position.x, targetY, transform.position.z);
+
+            if (transform.position.y >= initialY + liftDistance)
+                movingUp = false; // stop at max height
+        }
+        else if (movingDown)
+        {
+            targetY = Mathf.Max(transform.position.y - speed * Time.deltaTime, initialY);
+            transform.position = new Vector3(transform.position.x, targetY, transform.position.z);
+
+            if (transform.position.y <= initialY)
+                movingDown = false; // stop at min height
+        }
+    }
+
+    public void Lift()
+    {
+        movingUp = true;
+        movingDown = false; // cancel downward movement
+    }
+
+    public void Lower()
+    {
+        movingDown = true;
+        movingUp = false; // cancel upward movement
     }
 }
